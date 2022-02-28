457f5aa2b6249a8be909468d2547a66f854c8295
Emirhan Ataman <emirhan.ataman@modanisa.com>
2022-02-28 23:35:58 +0300

    cleanup config W.I.P.

--- adapters/rest/adapter.go
+++ adapters/rest/adapter.go
@@ -6,25 +6,27 @@ import (
 	"bit-driver-location-service/config"
 	"bit-driver-location-service/domain/driver"
 	"fmt"
-	"github.com/labstack/echo/v4"
 	"log"
 	"net/http"
+
+	"github.com/labstack/echo/v4"
 )
 
 type Adapter struct {
-	Config config.Server
+	Config *config.GeneralConfig
 	Logger *log.Logger
 	Server *echo.Echo
 }
 
 func (a *Adapter) Serve() {
-	var locationsColl = mongo.Connect().Collection("locations")
+	var locationsCollStr = "locations"
+	var locationsColl = mongo.Connect(a.Config.Database.DSN, locationsCollStr).Collection(locationsCollStr)
 	var driverRepository = driver.NewRepositoryDriver(locationsColl)
-	var driverService = driver.NewService(driverRepository)
+	var driverService = driver.NewService(driverRepository, a.Logger)
 	var driverRest = &handler.DriverHandler{Service: driverService}
 
 	a.Server.Add(http.MethodGet, "/nearest-driver-location", driverRest.FindDriverLocation)
 
-	a.Logger.Println("Server has started on port " + a.Config.Port)
-	a.Logger.Fatal(http.ListenAndServe(fmt.Sprintf("%s:%s", a.Config.Host, a.Config.Port), a.Server))
+	a.Logger.Println("Server has started on port " + a.Config.Server.Port)
+	a.Logger.Fatal(http.ListenAndServe(fmt.Sprintf("%s:%s", a.Config.Server.Host, a.Config.Server.Port), a.Server))
 }
