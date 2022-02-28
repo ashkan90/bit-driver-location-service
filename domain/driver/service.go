e8df2168c8c2f4aa812d97489e36285f93fad2d3
Emirhan Ataman <emirhan.ataman@modanisa.com>
2022-02-28 23:37:16 +0300

    add logger to service layer and fix zeroLen problem at findMin function

--- domain/driver/service.go
+++ domain/driver/service.go
@@ -4,10 +4,12 @@ import (
 	"bit-driver-location-service/request"
 	"bit-driver-location-service/response"
 	"github.com/umahmood/haversine"
+	"log"
 )
 
 type Service struct {
 	Repository *RepositoryDriver
+	logger     *log.Logger
 }
 
 type RepositoryImplementation interface {
@@ -19,13 +21,14 @@ type Distance struct {
 	Location   response.DriverLocation
 }
 
-func NewService(repo *RepositoryDriver) *Service {
-	return &Service{Repository: repo}
+func NewService(repo *RepositoryDriver, logger *log.Logger) *Service {
+	return &Service{Repository: repo, logger: logger}
 }
 
 func (s *Service) FindNearestDriverLocation(loc request.CustomerLocation) response.DriverLocation {
 	var drivers, err = s.Repository.FindNearestDriverByLocation(loc)
 	if err != nil {
+		s.logger.Println(err)
 		return response.DriverLocation{}
 	}
 
@@ -48,6 +51,9 @@ func (s *Service) FindNearestDriverLocation(loc request.CustomerLocation) respon
 }
 
 func findMin(distances []Distance) Distance {
+	if len(distances) == 0 {
+		return Distance{}
+	}
 	var min = distances[0]
 
 	for _, distance := range distances {
